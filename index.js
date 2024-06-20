c06c6311e93e70e364189b627abd903ba125532b
05_4 <69460076+05-4@users.noreply.github.com>
2024-06-20 21:46:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
-const data = [
-    { dataChunk: "Hello", connectionIndexes: [ { index: 1, weight: 35 }, { index: 2, weight: 8 }, { index: 3, weight: 16 } ] },
-    { dataChunk: ",", connectionIndexes: [ { index: 4, weight: 35 }, { index: 5, weight: 32 } ] },
-    { dataChunk: " mate", connectionIndexes: [ { index: 3, weight: 35 } ] },
-    { dataChunk: "!", connectionIndexes: [ { index: 7, weight: 35 }, { index: 22, weight: 23 } ] },
-    { dataChunk: " how", connectionIndexes: [ { index: 8, weight: 36 }, { index: 10, weight: 23 } ] },
-    { dataChunk: " what", connectionIndexes: [ { index: 15, weight: 35 } ] },
-    { dataChunk: "!", connectionIndexes: [  ] },
-    { dataChunk: " How", connectionIndexes: [ { index: 18, weight: 35 } ] },
-    { dataChunk: "'", connectionIndexes: [ { index: 9, weight: 35 } ] },
-    { dataChunk: "s", connectionIndexes: [ { index: 11, weight: 35 }, { index: 12, weight: 35 }, { index: 13, weight: 35 } ] },
-    { dataChunk: " are you", connectionIndexes: [ { index: 14, weight: 25 }, { index: 21, weight: 28 } ] },
-    { dataChunk: " it going", connectionIndexes: [ { index: 14, weight: 35 } ] },
-    { dataChunk: " life treating ya", connectionIndexes: [ { index: 14, weight: 35 } ] },
-    { dataChunk: " it hanging", connectionIndexes: [ { index: 14, weight: 35 } ] },
-    { dataChunk: "?", connectionIndexes: [ ] },
-    { dataChunk: "'", connectionIndexes: [ { index: 16, weight: 23 } ] },
-    { dataChunk: "s", connectionIndexes: [ { index: 17, weight: 28 } ] },
-    { dataChunk: " up mate", connectionIndexes: [ { index: 14, weight: 24 } ] },
-    { dataChunk: " are", connectionIndexes: [ { index: 19, weight: 37 }, { index: 20, weight: 34 } ] },
-    { dataChunk: " ya", connectionIndexes: [ { index: 14, weight: 24 }, { index: 21, weight: 23 } ] },
-    { dataChunk: " you", connectionIndexes: [ { index: 14, weight: 35 }, { index: 21, weight: 33 } ] },
-    { dataChunk: " today", connectionIndexes: [ { index: 14, weight: 10 } ] },
-    { dataChunk: "", connectionIndexes: [ ] },
-];
+const data = require("./model.json");
+const { Readable } = require("node:stream");
 
-const input = "Hello"; // Should be an even ", how are you?" or "!"
+const input = "Hello";
 const temperature = 1; // Max 2, min 1
 
+/*
+    * @param {Object} data
+    * @param {number} temp
+    * @returns {number}
+    * @description Get a random connection index.
+    * @example
+    * getConnection(data, 1);
+ */
 function getConnection(data, temp) {
     const randomIndex = Math.floor(Math.random() * data.connectionIndexes.length);
     const randomConnection = data.connectionIndexes[randomIndex];
@@ -40,32 +25,68 @@ function getConnection(data, temp) {
     return Math.random() < bias ? randomConnection.index : null;
 }
 
+/*
+    * @param {string} text
+    * @param {number} temp
+    * @returns {Promise<Readable>}
+    * @throws {Error}
+    * @description Process input text and generate a stream of data.
+    * @example
+    * processInput("Hello", 1).then((stream) => {
+    *     stream.on("data", (chunk) => {
+    *         console.log(chunk);
+    *     }
+    * });
+ */
 async function processInput(text, temp) {
+    if (!data || !Array.isArray(data) || !data.length) {
+        throw new Error("Model data is either empty or is not an array.");
+    }
+
+    if (!text)
+        throw new Error("Input text is empty.");
+    if (!temp || typeof temp !== "number" || temp < 1 || temp > 2)
+        throw new Error("Temperature must be a number and between 1 and 2.");
+
     let output = "";
     let completed = false;
     let connection = data.filter((v) => v.dataChunk === text)
         .sort(() => Math.random() - 0.5)[0];
 
-    while (!completed) {
-        const connectionIndex = getConnection(connection, temp);
-        const newConnection = data[connectionIndex];
+    const stream = new Readable({
+        read() {}
+    });
 
-        if (!newConnection) {
-            completed = true;
-            continue;
+    (() => {
+        stream.push(connection.dataChunk);
+
+        while (!completed) {
+            const connectionIndex = getConnection(connection, temp);
+            const newConnection = data[connectionIndex];
+
+            if (!newConnection) {
+                completed = true;
+                continue;
+            }
+
+            stream.push(newConnection.dataChunk);
+
+            output += newConnection.dataChunk;
+            connection = newConnection;
         }
 
-        process.stdout.write(connection.dataChunk);
-        await new Promise(res => setTimeout(res, Math.floor(Math.random() * 100)))
-
-        output += newConnection.dataChunk;
-        connection = newConnection;
-    }
-
-    process.stdout.write(connection.dataChunk);
-    console.log();
+        stream.push(null);
+    })();
     
-    return text + output;
+    return stream;
 }
 
-processInput(input, temperature);
+processInput(input, temperature).then((stream) => {
+    stream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    stream.on('end', () => {
+        process.stdout.write('\n');
+    });
+});
